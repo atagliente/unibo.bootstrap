d60aaa6b6ff68331589ffb995e80881071678742
Antonio Tagliente <antoniotagliente@aol.com>
2016-12-13 17:20:45 +0100

    homepage with template added

---
+++ Index/Home/old_version/calendar.js
@@ -0,0 +1,25 @@
+var DayEnum = {
+    LUN : {value: 0, name: "Lunedì"}
+    MAR : {value: 1, name: "Martedì"},
+    MER : {value: 2, name: "Mercoledì"},
+    GIO : {value: 3, name: "Giovedì"},
+    VEN : {value: 4, name: "Venerdì"},
+    SAB : {value: 5, name: "Sabato"},
+    DOM : {value: 6, name: "Domenica"}
+}
+
+var MonthEnum = {
+    GEN : {value: 0, name: "Gennaio"},
+    FEB : {value: 1, name: "Febbraio"},
+    MAR : {value: 2, name: "Marzo"},
+    APR : {value: 3, name: "Aprile"},
+    MAG : {value: 4, name: "Maggio"},
+    GIU : {value: 5, name: "Giugno"},
+    LUG : {value: 6, name: "Luglio"},
+    AGO : {value: 7, name: "Agosto"},
+    SET : {value: 8, name: "Settembre"},
+    OTT : {value: 9, name: "Ottobre"},
+    NOV : {value: 10, name: "Novembre"},
+    DIC : {value: 11, name: "Dicembre"}
+};
+
