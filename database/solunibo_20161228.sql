e467585b0eac18c91afeed2b8c64f0b7587a6269
Fabrizio Tasselli <fabrizio.tasselli@studio.unibo.it>
2016-12-28 14:16:54 +0100

    updated database

--- database/solunibo_20161228.sql
+++ database/solunibo_20161228.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Dic 28, 2016 alle 12:47
+-- Creato il: Dic 28, 2016 alle 13:14
 -- Versione del server: 5.7.14
 -- Versione PHP: 5.6.25
 
@@ -19,6 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `solunibo`
 --
+CREATE DATABASE IF NOT EXISTS `solunibo` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
+USE `solunibo`;
 
 -- --------------------------------------------------------
 
@@ -26,10 +28,11 @@ SET time_zone = "+00:00";
 -- Struttura della tabella `appelli_esami`
 --
 
-CREATE TABLE `appelli_esami` (
+CREATE TABLE IF NOT EXISTS `appelli_esami` (
   `id_apello` int(5) NOT NULL,
   `descrizione` varchar(500) COLLATE utf8_bin NOT NULL,
-  `fk_esami` int(20) NOT NULL
+  `fk_esami` int(20) NOT NULL,
+  PRIMARY KEY (`id_apello`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
@@ -48,12 +51,13 @@ INSERT INTO `appelli_esami` (`id_apello`, `descrizione`, `fk_esami`) VALUES
 -- Struttura della tabella `esami`
 --
 
-CREATE TABLE `esami` (
+CREATE TABLE IF NOT EXISTS `esami` (
   `id_esame` int(10) NOT NULL,
   `nome_esame` char(20) COLLATE utf8_bin NOT NULL,
   `nome_cognome_docente` char(30) COLLATE utf8_bin NOT NULL,
   `cfu` int(2) NOT NULL,
-  `anno` int(1) NOT NULL
+  `anno` int(1) NOT NULL,
+  PRIMARY KEY (`id_esame`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
@@ -70,7 +74,7 @@ INSERT INTO `esami` (`id_esame`, `nome_esame`, `nome_cognome_docente`, `cfu`, `a
 -- Struttura della tabella `evento`
 --
 
-CREATE TABLE `evento` (
+CREATE TABLE IF NOT EXISTS `evento` (
   `numero_progressivo` int(11) NOT NULL,
   `fk_studente` int(20) NOT NULL,
   `giorno` int(2) NOT NULL,
@@ -78,7 +82,8 @@ CREATE TABLE `evento` (
   `anno` int(4) NOT NULL,
   `descrizione_impegno` char(100) COLLATE utf8_bin NOT NULL,
   `ora` int(2) NOT NULL,
-  `minuti` int(2) NOT NULL
+  `minuti` int(2) NOT NULL,
+  PRIMARY KEY (`numero_progressivo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
@@ -97,11 +102,12 @@ INSERT INTO `evento` (`numero_progressivo`, `fk_studente`, `giorno`, `mese`, `an
 -- Struttura della tabella `insegnamenti`
 --
 
-CREATE TABLE `insegnamenti` (
+CREATE TABLE IF NOT EXISTS `insegnamenti` (
   `id_insegnamenti` int(10) NOT NULL,
   `nome_insegnamento` char(50) COLLATE utf8_bin NOT NULL,
   `docenti` char(50) COLLATE utf8_bin NOT NULL,
-  `fk_studente` int(20) NOT NULL
+  `fk_studente` int(20) NOT NULL,
+  PRIMARY KEY (`id_insegnamenti`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
@@ -120,9 +126,10 @@ INSERT INTO `insegnamenti` (`id_insegnamenti`, `nome_insegnamento`, `docenti`, `
 -- Struttura della tabella `logattemps`
 --
 
-CREATE TABLE `logattemps` (
+CREATE TABLE IF NOT EXISTS `logattemps` (
   `username` varchar(50) COLLATE utf8_bin NOT NULL,
-  `time` varchar(50) COLLATE utf8_bin NOT NULL
+  `time` varchar(50) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`time`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
@@ -144,10 +151,11 @@ INSERT INTO `logattemps` (`username`, `time`) VALUES
 -- Struttura della tabella `login`
 --
 
-CREATE TABLE `login` (
+CREATE TABLE IF NOT EXISTS `login` (
   `username` varchar(50) COLLATE utf8_bin NOT NULL,
   `password` char(128) COLLATE utf8_bin NOT NULL,
-  `fk_studente` int(20) NOT NULL
+  `fk_studente` int(20) NOT NULL,
+  PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
@@ -164,7 +172,7 @@ INSERT INTO `login` (`username`, `password`, `fk_studente`) VALUES
 -- Struttura della tabella `materiale_insegnamenti`
 --
 
-CREATE TABLE `materiale_insegnamenti` (
+CREATE TABLE IF NOT EXISTS `materiale_insegnamenti` (
   `numero_progressivo` int(11) NOT NULL,
   `nome_materiale` char(50) COLLATE utf8_bin NOT NULL,
   `link_materiale` char(200) COLLATE utf8_bin NOT NULL,
@@ -189,12 +197,13 @@ INSERT INTO `materiale_insegnamenti` (`numero_progressivo`, `nome_materiale`, `l
 -- Struttura della tabella `notifiche`
 --
 
-CREATE TABLE `notifiche` (
+CREATE TABLE IF NOT EXISTS `notifiche` (
   `numero_progressivo` int(100) NOT NULL,
   `descrizione` varchar(500) COLLATE utf8_bin NOT NULL,
   `letta` tinyint(1) NOT NULL,
   `nome_cognome_docente` varchar(30) COLLATE utf8_bin NOT NULL,
-  `fk_studente` int(20) NOT NULL
+  `fk_studente` int(20) NOT NULL,
+  PRIMARY KEY (`numero_progressivo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
@@ -203,12 +212,13 @@ CREATE TABLE `notifiche` (
 -- Struttura della tabella `studente`
 --
 
-CREATE TABLE `studente` (
+CREATE TABLE IF NOT EXISTS `studente` (
   `matricola` int(12) NOT NULL,
   `nome` char(30) COLLATE utf8_bin NOT NULL,
   `cognome` char(30) COLLATE utf8_bin NOT NULL,
   `data_di_nascita` char(20) COLLATE utf8_bin NOT NULL,
-  `fk_login` char(50) COLLATE utf8_bin NOT NULL
+  `fk_login` char(50) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`matricola`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
@@ -225,7 +235,7 @@ INSERT INTO `studente` (`matricola`, `nome`, `cognome`, `data_di_nascita`, `fk_l
 -- Struttura della tabella `tasse`
 --
 
-CREATE TABLE `tasse` (
+CREATE TABLE IF NOT EXISTS `tasse` (
   `numero_progressivo` int(5) NOT NULL,
   `anno_di_corso` char(20) COLLATE utf8_bin NOT NULL,
   `monorata` char(200) COLLATE utf8_bin NOT NULL,
@@ -233,7 +243,8 @@ CREATE TABLE `tasse` (
   `seconda_rata` char(200) COLLATE utf8_bin NOT NULL,
   `terza_rata` char(200) COLLATE utf8_bin NOT NULL,
   `mora` char(200) COLLATE utf8_bin NOT NULL,
-  `fk_studente` int(11) NOT NULL
+  `fk_studente` int(11) NOT NULL,
+  PRIMARY KEY (`numero_progressivo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
@@ -244,64 +255,6 @@ INSERT INTO `tasse` (`numero_progressivo`, `anno_di_corso`, `monorata`, `prima_r
 (1, '1', 'Iscrizione con monorata\r\nImporto €1980.15\r\nScadenza 30/09/2011\r\nPagata 29/09/2011', 'Prima rata\r\nImporto €600.00\r\nScadenza 30/09/2011\r\nPagata', 'Seconda rata\r\nImporto €728.00\r\nScadenza 29/12/2011\r\nPagata', 'Terza rata\r\nImporto €252.00\r\nScadenza 18/05/2012\r\nPagata', '', 123456),
 (2, '2', 'Iscrizione con monorata\r\nImporto €1890.00\r\nScadenza 30/09/2012\r\nPagata', 'Seconda rata\r\nImporto €600.00\r\nScadenza 30/09/2012\r\nPagata 30/09/2012', 'Seconda rata\r\nImporto €724.00\r\nScadenza 30/12/2012\r\nPagata 30/12/2012', 'Terza rata\r\nImporto €201.00\r\nScadenza 30/09/2012\r\nPagata 30/05/2012', '', 123456),
 (3, '3', 'Iscrizione con monorata\r\nImporto €1744.00\r\nScadenza 30/09/2013\r\nPagata', 'Prima rata\r\nImporto €600.00\r\nScadenza 30/09/2013\r\nPagata 30/09/2013', 'Seconda rata\r\nImporto €744.00\r\nScadenza 30/12/2013\r\nPagata 15/12/2013', 'Terza rata\r\nImporto €231.00\r\nScadenza 30/05/2014\r\nPagata 22/05/2014', 'Mora per pagamento in ritardo\r\nImporto €100.00\r\nPagata 01/01/2014', 123456);
-
---
--- Indici per le tabelle scaricate
---
-
---
--- Indici per le tabelle `appelli_esami`
---
-ALTER TABLE `appelli_esami`
-  ADD PRIMARY KEY (`id_apello`);
-
---
--- Indici per le tabelle `esami`
---
-ALTER TABLE `esami`
-  ADD PRIMARY KEY (`id_esame`);
-
---
--- Indici per le tabelle `evento`
---
-ALTER TABLE `evento`
-  ADD PRIMARY KEY (`numero_progressivo`);
-
---
--- Indici per le tabelle `insegnamenti`
---
-ALTER TABLE `insegnamenti`
-  ADD PRIMARY KEY (`id_insegnamenti`);
-
---
--- Indici per le tabelle `logattemps`
---
-ALTER TABLE `logattemps`
-  ADD PRIMARY KEY (`time`);
-
---
--- Indici per le tabelle `login`
---
-ALTER TABLE `login`
-  ADD PRIMARY KEY (`username`);
-
---
--- Indici per le tabelle `notifiche`
---
-ALTER TABLE `notifiche`
-  ADD PRIMARY KEY (`numero_progressivo`);
-
---
--- Indici per le tabelle `studente`
---
-ALTER TABLE `studente`
-  ADD PRIMARY KEY (`matricola`);
-
---
--- Indici per le tabelle `tasse`
---
-ALTER TABLE `tasse`
-  ADD PRIMARY KEY (`numero_progressivo`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
