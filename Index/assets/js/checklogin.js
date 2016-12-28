53ef44a9dd4d9bd165b27aa448af800752637dad
Antonio Tagliente <antoniotagliente@aol.com>
2016-12-28 11:58:27 +0100

    calendar added

--- Index/assets/js/checklogin.js
+++ Index/assets/js/checklogin.js
@@ -6,6 +6,8 @@ window.addEventListener('load', function () {
     $.get( "../assets/php/LoginCheck.php", function(data) {
         if (data != "accept") location.replace(data);
     });
+
+    $(".addControls").addEventListener()
 })
 
 
