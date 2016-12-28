00f1383c2ea52d06834f938c14b991c2e178bf8a
Fabrizio Tasselli <fabrizio.tasselli@studio.unibo.it>
2016-12-28 17:50:17 +0100

    another php test

---
+++ Index/assets/php/esami.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Fabrizio
+ * Date: 28-Dec-16
+ * Time: 15:21
+ */
+
+//    $dbConnection = DBConnection::getInstance();
+//    $sql = "SELECT matricola FROM login, studente WHERE username = fk_login";
+//    $conn = $dbConnection->getConnection();
+//    $result = $conn->query($sql);
+//
+//    if($result->num_rows > 0) {
+//        while($row = $result->fetch_assoc()) {
+//            echo $row["matricola"];
+//        }
+//    } else {
+//        echo "0 results";
+//    }
+    echo "ASDLKFJSDLFJSDLFKDJSFLSDKJFSDLKFJSDLKFJSDLKFJSDLKFJSDLKFJSDLKFJSDLKJFLSDKJFLSKDJF";
+
+?>
