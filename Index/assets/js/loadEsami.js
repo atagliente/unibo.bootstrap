241cf7be98b614be140bd35d9c2ee02d49f2a31b
Giacomo Venturini <giacomo.venturini3@gmail.com>
2016-12-29 17:31:23 +0100

    little progress with conversion php->JSON->javascript

--- Index/assets/js/loadEsami.js
+++ Index/assets/js/loadEsami.js
@@ -1,5 +1,15 @@
-window.addEventListener('load', function () {
-    $.get( "../assets/php/esami.php", function(data) {
-        alert(data);
-    });
+window.addEventListener('load', function() {
+  
+      $.get("../assets/php/esami.php", function(data) {
+      //  alert(data);
+        //var obj = JSON.parse(data);
+
+        alert(JSON.parse(data));
+
+        //  var array = data[0][0];
+        //  alert(array);
+/*
+          var json = JSON.parse(data);
+          alert(json); */
+      });
 })
--- Index/assets/php/esami.php
+++ Index/assets/php/esami.php
@@ -20,10 +20,9 @@
 
 
       $dbConnection = DBConnection::getInstance();
-
+      $conn = $dbConnection->getConnection();
 
       $sql = "SELECT matricola FROM studente WHERE fk_login = '$userEmail'";
-      $conn = $dbConnection->getConnection();
       $result = $conn->query($sql);
 
       if($result->num_rows == 1) {
@@ -35,8 +34,45 @@
           echo "errore 0 results";
       }
 
+      $sql = "SELECT * FROM esami WHERE fk_studente = '$matricola'";
+      $result = $conn->query($sql);
+
+      $i = 0;
+      $count = 0;
+
+      if($result->num_rows > 0) {
+          while($row = $result->fetch_assoc()) {
+
+            //echo json_encode($row);
 
 
+            foreach ($row as $value){
+              //echo $value . " ";
+              $riga[$i] = $value;
+              $i = $i + 1;
+
+            }
+
+
+
+        //    echo $riga[0] . "  " . $riga[1] . "  " . $riga[2] . "  " . $riga[3] . "  " . $riga[4] . "  " . $riga[5] . "\n";
+            $array_esami[$count] = $row;
+
+
+            $array[$count] = $riga;
+            $i = 0;
+            $count = $count + 1;
+          }
+      } else {
+          echo "errore 0 results";
+      }
+
+      //unica riga
+      echo json_encode($riga);
+    
+    //  echo $array[0][0];
+    //  echo $array_esami[0]["id_esame"];
+    // echo $array;
 
 /*
           $sql = "SELECT matricola FROM login, studente WHERE username = fk_login";
