ae8d17022fa820a9ba503d085953cb775983cc12
Antonio Tagliente <antoniotagliente@aol.com>
2017-01-04 11:50:32 +0100

    testResult.php added

--- Index/assets/php/pageRequest/testResult.php
+++ Index/assets/php/pageRequest/testResult.php
@@ -4,7 +4,7 @@ require('../MakeOperationsOnDB.php');
 
 $var = new MakeOperationsOnDB();
 
-$sql = "SELECT test.*, teacher.firstName, teacher.lastName FROM `test` JOIN `teacher` JOIN `exam` WHERE teacher.numberID = test.teacher___fk AND exam.numberID = exam___fk";
+$sql = "SELECT test.*, teacher.firstName, teacher.lastName FROM `test` JOIN `storyboard` ON storyBoard.test___fk != test.numberID JOIN `teacher` ON test.numberID ==  JOIN `exam`  AND teacher.numberID = test.teacher___fk AND exam.numberID = exam___fk";
 
 echo $var->getJSONFromQuery("test", $sql);
 
