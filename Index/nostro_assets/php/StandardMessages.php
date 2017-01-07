870fac2d18fe40bcf25759f524a6fb557e99169e
Giacomo Venturini <giacomo.venturini3@gmail.com>
2017-01-07 12:15:39 +0100

    minor fixes

--- Index/nostro_assets/php/StandardMessages.php
+++ Index/nostro_assets/php/StandardMessages.php
@@ -5,7 +5,7 @@ abstract class StandardMessages
     const USER_WRONG = "Lo user non esiste";
     const PW_WRONG = "La password non corrisponde";
     const MAX_ATTEPS = "Tentativi massimi raggiunti, l'evento sarà notificato all'amministratore";
-    const SUCCESS_REDIRECT = "../../../Home/index.html";
+    const SUCCESS_REDIRECT = "../../../Home/home.html";
     const ALERT_MAIL = "Sono stati superati 5 tentativi d'accesso al account: ";
     const INSUCCESS_REDIRECT = '../Login/login.html';
     const SESSION_ID = "sec_session_id";
