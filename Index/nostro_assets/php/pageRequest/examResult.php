2e20943ae27588a650e286231ce73a7f7d6a2302
Antonio Tagliente <antoniotagliente@aol.com>
2017-01-11 16:58:55 +0100

    exam page finished

--- Index/nostro_assets/php/pageRequest/examResult.php
+++ Index/nostro_assets/php/pageRequest/examResult.php
@@ -4,7 +4,7 @@ require('../MakeOperationsOnDB.php');
 
 $var = new MakeOperationsOnDB();
 
-$sql = "SELECT exam.*, storyBoard.vote, storyBoard.prenotation FROM `storyBoard` JOIN `test` ON storyBoard.test___fk = test.numberID JOIN `exam` ON test.exam___fk = exam.numberID";
+$sql = "SELECT exam.*, storyboard.vote, storyboard.prenotation FROM `storyboard` JOIN `test` ON storyboard.test___fk = test.numberID JOIN `exam` ON test.exam___fk = exam.numberID";
 
 echo $var->getJSONFromQuery("exam", $sql);
 
--- Index/nostro_assets/php/pageRequest/testResult.php
+++ Index/nostro_assets/php/pageRequest/testResult.php
@@ -4,7 +4,7 @@ require('../MakeOperationsOnDB.php');
 
 $var = new MakeOperationsOnDB();
 
-$sql = "SELECT test.*, teacher.firstName, teacher.lastName, storyBoard.vote, storyBoard.prenotation FROM `storyBoard` JOIN `test` ON storyBoard.test___fk = test.numberID JOIN `exam` ON test.exam___fk = exam.numberID JOIN `teacher` ON exam.teacher___fk = teacher.numberID";
+$sql = "SELECT test.*, teacher.firstName, teacher.lastName, storyboard.vote, storyboard.prenotation FROM `storyboard` JOIN `test` ON storyboard.test___fk = test.numberID JOIN `exam` ON test.exam___fk = exam.numberID JOIN `teacher` ON exam.teacher___fk = teacher.numberID";
 
 echo $var->getJSONFromQuery("test", $sql);
 
