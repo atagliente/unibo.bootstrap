856cf0f52172d65052ee244c034cb0957127d5b8
Fabrizio Tasselli <fabrizio.tasselli@studio.unibo.it>
2017-01-14 09:53:52 +0100

    restored db and query of teacher table

--- Index/nostro_assets/php/pageRequest/teachingsResult.php
+++ Index/nostro_assets/php/pageRequest/teachingsResult.php
@@ -4,7 +4,7 @@ require('../MakeOperationsOnDB.php');
 
 $var = new MakeOperationsOnDB();
 
-$sql = "SELECT * FROM `teacher` JOIN `teachingdoc` ON teacher.teacherID = teachingdoc.teacher___fk JOIN `exam` ON teacher.teacherID = exam.teacher___fk ORDER BY `teachingdoc`.`teachingID`  DESC";
+$sql = "SELECT * FROM `teacher` JOIN `teachingdoc` ON teacher.numberID = teachingdoc.teacher___fk JOIN `exam` ON teacher.numberID = exam.teacher___fk ORDER BY `teachingdoc`.`teachingID` DESC";
 
 echo $var->getJSONFromQuery("teachings", $sql);
 
