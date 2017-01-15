89712d8410ffa42681fd00796c4dd942c20290b9
Fabrizio Tasselli <fabrizio.tasselli@studio.unibo.it>
2017-01-15 17:12:12 +0100

    fixed esami.css and pageGenerator.js

--- Index/nostro_assets/js/pageGenerator.js
+++ Index/nostro_assets/js/pageGenerator.js
@@ -30,20 +30,22 @@ $(function () {
             var div;
             var exam = "<div class=\"panel panel-default\"> " +
                 "           <div class=\"panel-heading\"> " +
-                "               <h4 class=\"panel-title\"> " +
-                "                   <a data-toggle=\"collapse\" " + "    data-target=#collapse" + numberID + ">" + title + "" +
+                "               <h2 class=\"panel-title\"> " +
+                "                   <a data-toggle=\"collapse\" " + "    href=#" + numberID + ">" + title + "" +
                 "                   </a> " +
-                "               </h4> " +
-                "               <div id=\"collapse" + numberID + "\" class= \"panel-collapse collapse\"> " +
-                "                   <div id=\"" + numberID + "\" class=\" panel-body\">" +
+                "               </h2> " +
+                "            </div>" +
+                "            <div id=\"" + numberID + "\" class= \" panel-collapse collapse in\"> " +
+                "               <div class=\"panel-body\">" +
                 "               </div>" +
-                "               </div>" +
+                "            </div>" +
+                "           " +
                 "       </div>";
             if(vote > 0) {
                 div = document.getElementById('verbalized');
             } else if (prenotation == 0) {
                 div = document.getElementById('restanti');
-            } else div = document.getElementById('prenotati');
+            } else div = document.getElementById('prenotati').childNodes[0];
             div.insertAdjacentHTML( 'afterbegin', exam);
         }
     });
